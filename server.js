763e35736a0387a82b806a9c11b61cbdbf1df49d
urakhimbayev-prog <u.rakhimbayev@gmail.com>
2026-03-31 19:39:19 +0500

    server.js

--- server.js
+++ server.js
@@ -88,4 +88,4 @@ app.get("/earthquakes", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API Server: http://localhost:${PORT}/earthquakes/today`));
+app.listen(PORT, () => console.log(`API Server: http://localhost:${PORT}/earthquakes`));
